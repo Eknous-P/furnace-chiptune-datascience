94bf5708df58d4013de0984257cfd4cd5711ab88
Eknous-P <ehovakimyan053@gmail.com>
2025-04-27 17:45:26 +0400

    it scrapes good

--- scraper.py
+++ scraper.py
@@ -6,37 +6,78 @@ import google_auth_oauthlib.flow
 import googleapiclient.discovery
 import googleapiclient.errors
 
+import json
+
 scopes = ["https://www.googleapis.com/auth/youtube.readonly"]
 
+ASH_PLAYLIST_ID = "PLM98E7f5OROYhVKUz15lLpacLiQqmMU-v"
+
 CREDENTIALS_FILE = "secrets/client_secret_file.json"
+OUTPUT_FILE = "data.json"
+PRINT_FILE = "test.txt"
 
-def request(playlist, count):
-  # Disable OAuthlib's HTTPS verification when running locally.
-  # *DO NOT* leave this option enabled in production.
-  # this shall only be run locally so it can stay
-  os.environ["OAUTHLIB_INSECURE_TRANSPORT"] = "1"
 
-  api_service_name = "youtube"
-  api_version = "v3"
-  client_secrets_file = CREDENTIALS_FILE
+# Disable OAuthlib's HTTPS verification when running locally.
+# *DO NOT* leave this option enabled in production.
+# this shall only be run locally so it can stay
+os.environ["OAUTHLIB_INSECURE_TRANSPORT"] = "1"
 
-  # Get credentials and create an API client
-  flow = google_auth_oauthlib.flow.InstalledAppFlow.from_client_secrets_file(
-    client_secrets_file, scopes)
-  credentials = flow.run_console()
-  youtube = googleapiclient.discovery.build(
-    api_service_name, api_version, credentials=credentials)
+class api:
+  def __init__(self, credf):
+    self.api_service_name = "youtube"
+    self.api_version = "v3"
+    self.client_secrets_file = credf
+  def auth(self):
+    self.flow = google_auth_oauthlib.flow.InstalledAppFlow.from_client_secrets_file(
+      self.client_secrets_file, scopes)
+    self.credentials = self.flow.run_console()
+    self.youtube = googleapiclient.discovery.build(
+      self.api_service_name, self.api_version, credentials=self.credentials)
+  def requestPlaylist(self, playlist, count):
+    request = self.youtube.playlistItems().list(
+      part="snippet,contentDetails",
+      maxResults=count,
+      playlistId=playlist
+    )
+    response = request.execute()
+    return response
+  def requestVideo(self, video):
+    request = self.youtube.videos().list(
+      part="snippet,contentDetails,statistics",
+      id=video
+    )
+    response = request.execute()
+    return response
 
-  request = youtube.playlistItems().list(
-    part="snippet,contentDetails",
-    maxResults=count,
-    playlistId=playlist
-  )
-  response = request.execute()
-
-  return response
 
 if __name__ == "__main__":
-  request("PLM98E7f5OROYhVKUz15lLpacLiQqmMU-v", 5)
-  
-    # "PLM98E7f5OROYhVKUz15lLpacLiQqmMU-v"
+  yt = api(CREDENTIALS_FILE)
+  yt.auth()
+
+  playlistJson = yt.requestPlaylist(ASH_PLAYLIST_ID, 100)
+  vidIds=""
+  for i in playlistJson["items"]:
+    id = i["contentDetails"]["videoId"]
+    print("video id: {0}".format(id))
+    vidIds+=id+','
+  vidIds = vidIds.removesuffix(',')
+
+  print("ids {0}".format(vidIds))
+
+  vids = yt.requestVideo(vidIds)
+  for i in vids["items"]:
+    with open(PRINT_FILE, "a") as f:
+      print(("VIDEO DETAILS FOR {0}:\n"+
+          " title: {1}\n"+
+          " author: {2}\n"+
+          " views: {3}\n"+
+          " likes: {4}\n"+
+          " uploadDate: {5}\n"
+          ).format(
+            i["id"],
+            i["snippet"]["title"],
+            i["snippet"]["channelTitle"],
+            i["statistics"]["viewCount"],
+            i["statistics"]["likeCount"],
+            i["snippet"]["publishedAt"]
+          ),file=f)

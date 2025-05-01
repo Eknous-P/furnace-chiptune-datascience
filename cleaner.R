9198240cdde0c7109a7b11fc6d4a5c02d6b5ab68
Eknous-P <ehovakimyan053@gmail.com>
2025-05-01 15:01:11 +0400

    more cleanup, fully move plots and a sensible plot

--- cleaner.R
+++ cleaner.R
@@ -6,23 +6,38 @@ mus_initial_data <- read_delim(
   "./data.csv",
   delim="/",
   show_col_types = FALSE
-)|>
-  clean_names()
+)|>clean_names()
 
 mus_clean_data <- mus_initial_data|>
   mutate(
     vid_uploaddate = as.Date(vid_uploaddate),
   )|>
   filter(
-    as.numeric(format(vid_uploaddate, "%Y")) > 2021       # remove videos from before furnace
+    vid_uploaddate > as.Date("2021-12-31") # remove videos from before furnace v0.2 released
   )|>
-  arrange(vid_uploaddate)|>                                # arrange by upload date
-  mutate(n = row_number(), .before=vid_id)
+  filter(
+    vid_id != c(
+      "_67sF4onNQM", # 8 bit guy second reality on x16 video
+      "ZqJC7XBihm0" # revision demoparty '25 official video
+    )
+  )|>
+  arrange(vid_uploaddate)|>                # arrange by upload date
+  mutate(n = row_number(), .before=vid_id) # index chronologically
 
 # TODO: remove more videos
 # maybe consult server
 
 mus_views_high <- mus_clean_data|>
   filter(
-    vid_stat_view>100000
+    vid_stat_view > 100000
+  )
+
+mus_views_medium <- mus_clean_data|>
+  filter(
+    vid_stat_view < 100000 & vid_stat_view > 10000
+  )
+
+mus_views_verylow <- mus_clean_data|>
+  filter(
+    vid_stat_view < 100
   )
--- plots.R
+++ plots.R
@@ -1,3 +1,56 @@
 source("cleaner.R")
+library(rlang)
 
-mus_clean_data|>print()
+mus_clean_data|>ggplot(
+  aes(
+    x = vid_uploaddate,
+    y = n
+  )
+)+
+geom_point(
+  alpha = .5,
+  size = 1
+)+
+scale_x_date()-> plot_upload_hist
+
+mus_clean_data|>
+arrange(vid_stat_view)|>
+tail(20)|>
+ggplot(
+  aes(
+    x = vid_stat_view,
+    y = vid_title,
+    fill = vid_auth
+  )
+)+
+scale_y_discrete(
+  name = "Title",
+  labels = as_function(~ strtrim(.,20))
+)+
+geom_col(
+  aes(
+    group = vid_stat_view
+  )
+)+
+theme(
+  legend.position = "bottom",
+  legend.title.position = "top"
+)+
+labs(
+  title = "Top 20 Most Viewed Videos in the Playlist",
+  y = "Video Title",
+  x = "Video Views",
+  fill = "Video Author (channel)"
+)-> plot_most_viewed
+
+
+mus_views_high|>ggplot(
+  aes(
+    y = vid_stat_view,
+    x = vid_auth
+  )
+)+
+geom_point(
+  alpha = 1,
+  size = 2,
+) -> plot_views_high

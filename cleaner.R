59f8c84a7578e40b088e8cec21179ea65bbd6100
Eknous-P <ehovakimyan053@gmail.com>
2025-05-02 22:57:46 +0400

    todos

--- cleaner.R
+++ cleaner.R
@@ -2,6 +2,7 @@ library(tidyverse)
 library(readr)
 library(janitor)
 library(lubridate)
+library(stringi)
 
 mus_initial_data <- read_delim(
   "./data.csv",
@@ -17,14 +18,16 @@ mus_clean_data <- mus_initial_data|>
   filter(
     vid_uploaddate > as.Date("2021-12-31") # remove videos from before furnace v0.2 released
   )|>
-  filter(
-    vid_id != c(
+  filter( # TODO: FIX
+    stri_detect_regex(vid_id, c(
       "_67sF4onNQM", # 8 bit guy second reality on x16 video
       "ZqJC7XBihm0" # revision demoparty '25 official video
-    ) || 0 # sneaky workaround ?
+    ),TRUE) || 0
   )|>
   arrange(vid_uploaddate)|>                # arrange by upload date
   mutate(n = row_number(), .before=vid_id) # index chronologically
+
+searchfor_title <- function (t) mus_clean_data|>filter(stri_detect_regex(vid_title, t))
 
 # TODO: remove more videos
 # maybe consult server

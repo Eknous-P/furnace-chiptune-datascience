5bf911c1add0d4e34d6b3c744cd5281b5f9a40bd
Eknous-P <ehovakimyan053@gmail.com>
2025-05-05 15:15:17 +0400

    fix fix fix

--- plot_click.js
+++ plot_click.js
@@ -1,12 +1,7 @@
 function (el, x, data) {
-  const logs = false;
-  
   el.on('plotly_click', function(d) {
-    if (logs) console.log(d);
     // very hacky but works lmao
-    if (logs) let id = d.points[0].value - 1;
-    if (logs) console.log(id);
-    if (logs) console.log(data);
+    let id = d.points[0].value - 1;
     let url = 'https://youtu.be/' + data[Math.floor(id)].id;
     window.location = url;
   })
--- plots/plot_perChipUsage.R
+++ plots/plot_perChipUsage.R
@@ -18,18 +18,5 @@ labs(
 ggplotly(
   p = plot_perChip,
   dynamic_axes = TRUE,
-  tooltip = "text"
-)%>%
-  # js function to open the video when clicked on it
-  onRender( # TODO: fix
-    "
-  function (el, x, data) {
-    el.on('plotly_click', function(d) {
-      console.log(d)
-      console.log(data)
-    })
-  }
-  ", data = data_frame (
-    id = data_topViews$vid_id
-  )
-  )-> plot_perChip
+  tooltip = c("color","y")
+) -> plot_perChip
